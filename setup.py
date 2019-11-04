0393979331682aab4229509a5e7bec4bb145d027
Darcy.G <darcy.gong@gmail.com>
2019-11-04 23:17:51 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -127,11 +127,12 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
 
-    python_requires='>=3.6, < 4',
+    python_requires='>=3.5, < 4',
 
     # This field adds keywords for your project which will appear on the
     # project page. What does your project relate to?
